bfaeb0ac47920c45a09a35d11c3fddaff3863bae
cybergausho <lauttmz@gmail.com>
2024-09-12 05:54:09 -0300

    avances sso

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,23 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/google-auth/redirect', function () {
+    return Socialite::driver('google')->redirect();
+});
+ 
+Route::get('/google-auth/callback', function () {
+    $user = Socialite::driver('google')->user();
+    dd($user);
+    // $user->token
+});
+
+Route::get('/hola-mundo', function () {
+    return "Hola mundo";
+
 });
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,19 @@
 <?php
-
+use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
+});
+
+Route::get('/google-auth/redirect', function () {
+    return Socialite::driver('google')->redirect();
+});
+ 
+Route::get('/google-auth/callback', function () {
+    $user = Socialite::driver('google')->user();
+    dd($user);  
+    // $user->token
 });
 
 require __DIR__.'/auth.php';
